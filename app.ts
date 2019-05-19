8c2262674f551b6a5381740322b588cfd1822fe4
HairyMaclary <cliff.hamilton@outlook.com>
2019-05-19 19:09:04 +1200

    Lec 71 complete

--- app.ts
+++ app.ts
@@ -1,27 +1,14 @@
-    // Exercise 1 - Maybe use an Arrow Function?
-    const double = (value: number) => value * 2;
-    console.log(double(10));
-     
-    // Exercise 2 - If only we could provide some default values...
-    const greet = (name:string = 'max') => console.log("Hello, " + name);
-    greet();
-    greet("Anna");
-     
-    // Exercise 3 - Isn't there a shorter way to get all these values?
-    const numbers = [-3, 33, 38, 5];
-    console.log(Math.min(...numbers));
-     
-    // Exercise 4 - I have to think about Exercise 3 ...
-    let newArray = [55, 20];
-    newArray = [...newArray, ...numbers];
-    console.log(newArray);
-     
-    // Exercise 5 - That's a well-constructed array.
-    const testResults = [3.89, 2.99, 1.38];
-    const [ result1, result2, result3 ] = testResults;
-    console.log(result1, result2, result3);
-     
-    // Exercise 6 - And a well-constructed object!
-    const scientist = {firstName: "Will", experience: 12};
-    const { firstName, experience } = scientist;
-    console.log(firstName, experience);
+namespace MyMath {
+    const PI = 3.142;
+    
+    export function calcCirc(diam: number): number {
+        return diam * PI;
+    }
+
+    export function calcRecArea(width: number, height: number): number {
+        return width * height;
+    }
+}
+
+const PI = 3.1419; // no redeclaration error
+console.log(MyMath.calcRecArea(10,20));
