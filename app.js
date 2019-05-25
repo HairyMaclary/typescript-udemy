5714859cd5e9bd7e81cf31f12afcd1630104d2f6
HairyMaclary <cliff.hamilton@outlook.com>
2019-05-26 09:28:52 +1200

    Generics completed

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var myMath_1 = require("./myMath");
-var PI = 3.1419; // no redeclaration error
-console.log(PI);
-console.log(myMath_1.calcRecArea(10, 20));
+class MyMap {
+    constructor() {
+        this.mapArray = [];
+    }
+    setItem(key, item) {
+        const entry = [key, item];
+        this.mapArray.push(entry);
+    }
+    getItem(key) {
+        return this.mapArray.find((element) => element[0] === key);
+    }
+    clear() {
+        this.mapArray = [];
+    }
+    printMap() {
+        this.mapArray.forEach(element => console.log(element[0], element[1]));
+    }
+}
+const numberMap = new MyMap();
+numberMap.setItem('apples', 5);
+numberMap.setItem('bananas', 10);
+numberMap.printMap();
+const stringMap = new MyMap();
+stringMap.setItem('name', "Max");
+stringMap.setItem('age', "27");
+stringMap.printMap();
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,6 +1,34 @@
 
-import { calcRecArea } from './myMath';
+class MyMap<T> {
 
-const PI = 3.1419; // no redeclaration error
-console.log(PI);
-console.log(calcRecArea(10,20));
+    private mapArray: (string | T)[][] = [];
+
+    setItem(key: string, item: T) {
+        const entry = [key, item];
+        this.mapArray.push(entry);
+    }
+
+    getItem(key: string) {
+        return this.mapArray.find( (element) => element[0] === key );
+    }
+
+    clear() {
+        this.mapArray = [];
+    }
+
+    printMap() {
+        this.mapArray.forEach(
+            element => console.log(element[0], element[1])
+        );
+    }
+}
+
+const numberMap = new MyMap<number>();
+numberMap.setItem('apples', 5);
+numberMap.setItem('bananas', 10);
+numberMap.printMap();
+ 
+const stringMap = new MyMap<string>();
+stringMap.setItem('name', "Max");
+stringMap.setItem('age', "27");
+stringMap.printMap();
