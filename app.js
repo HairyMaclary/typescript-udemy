84f04be709555b9c7b7579553439492dcac7aeeb
HairyMaclary <cliff.hamilton@outlook.com>
2019-06-14 21:08:26 +1200

    card average

--- app.js
+++ app.js
@@ -1,130 +1,46 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
+const chanceSame = (numPairs) => {
+    if (numPairs === 1)
+        return 0.5;
+    return 1 / (numPairs * 2 - 1);
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+const isMatch = (pairCount) => {
+    const randomNumber = Math.random();
+    const matchProb = chanceSame(pairCount);
+    if (matchProb > randomNumber)
+        return true;
+    else
+        return false;
 };
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-function logged(constructorFn) {
-    console.log(constructorFn);
-}
-//Think of the '@' as 'attach' a funciton
-let Person = class Person {
-    constructor() {
-        console.log('Hi');
-    }
-};
-Person = __decorate([
-    logged,
-    __metadata("design:paramtypes", [])
-], Person);
-function logging(value) {
-    return value ? logged : () => { };
-}
-// logging is not a decorator. We execute it and then a valid decorator is returned and 'attached'.
-let Car = class Car {
-    constructor() {
-        console.log('Hello from a car');
-    }
-};
-Car = __decorate([
-    logging(true),
-    __metadata("design:paramtypes", [])
-], Car);
-function printable(constructorFn) {
-    constructorFn.prototype.print = function () {
-        console.log(this);
-    };
-}
-let Plant = class Plant {
-    constructor() {
-        this.name = "Green Plant";
-    }
-};
-Plant = __decorate([
-    logging(true),
-    printable
-], Plant);
-const plant = new Plant();
-// forced to cast due to typescript bug
-plant.print();
-// Decorator Factory
-function editable(value) {
-    return function (target, propName, descriptor) {
-        target;
-        propName;
-        descriptor.writable = value;
-    };
-}
-function overwritable(value) {
-    return function (target, propName) {
-        target;
-        propName;
-        const newDescriptor = {
-            writable: value
-        };
-        return newDescriptor;
-    };
-}
-class Project {
-    constructor(name) {
-        this.projectName = name;
-    }
-    // want to add some meta data to this method to make it non-overwritable
-    calcBudget() {
-        console.log(1000);
-    }
-}
-__decorate([
-    editable(false),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", []),
-    __metadata("design:returntype", void 0)
-], Project.prototype, "calcBudget", null);
-const project = new Project("Super Project");
-project.calcBudget();
-// project.calcBudget = function() {
-//     console.log(2000);
-// };
-// project.calcBudget();
-// while this is decorator you can still use factories with parameter decorators
-function printInfo(target, methodName, paramIndex) {
-    console.log('Target: ', target);
-    console.log('Method Name:', methodName);
-    console.log('Parameter Index:', paramIndex);
-}
-class Course {
-    constructor(name) {
-        this.name = name;
-    }
-    printStudentNumbers(mode, printAll) {
-        mode;
-        if (printAll) {
-            console.log(10000);
+const runGame = () => {
+    let pairsRemaining = 26;
+    let count = 0;
+    while (pairsRemaining > 0) {
+        const pairFound = isMatch(pairsRemaining);
+        if (pairFound) {
+            pairsRemaining -= 1;
         }
-        else {
-            console.log(2000);
-        }
+        count++;
     }
-}
-__decorate([
-    __param(1, printInfo),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", [String, Boolean]),
-    __metadata("design:returntype", void 0)
-], Course.prototype, "printStudentNumbers", null);
-const course = new Course('Super Course');
-course.printStudentNumbers('anything', true);
-course.printStudentNumbers('anything', false);
-// Target:  Object { … }
-// Method Name: printStudentNumbers
-// Parameter Index: 1
-// 10000
-// 2000
+    return count;
+};
+const collectAverage = () => {
+    let testRunCount = 100000;
+    let results = [];
+    let avg;
+    while (testRunCount > 0) {
+        let turns = runGame();
+        if (typeof turns === 'number')
+            results.push(turns);
+        testRunCount--;
+    }
+    if (results.length) {
+        const sum = results.reduce(function (a, b) { return a + b; });
+        avg = sum / results.length;
+    }
+    else
+        avg = 0;
+    console.log('ave:', avg);
+};
+collectAverage();
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,120 +1,49 @@
-
-function logged(constructorFn: Function) {
-    console.log(constructorFn);
+const chanceSame = (numPairs: number): number => {
+    if (numPairs === 1) return 0.5;
+    return 1 / (numPairs * 2 - 1);
 }
 
-//Think of the '@' as 'attach' a funciton
-@logged
-class Person {
-    constructor() {
-        console.log('Hi');
-    }
+const isMatch = (pairCount: number): boolean => {
+    const randomNumber = Math.random();
+    const matchProb = chanceSame(pairCount);
+    if (matchProb > randomNumber) return true;
+    else return false;
 }
 
-function logging(value: boolean) {
-    return value ? logged : ()=>{};
-}
+const runGame = () => {
 
-// logging is not a decorator. We execute it and then a valid decorator is returned and 'attached'.
-@logging(true)
-class Car {
-    constructor() {
-        console.log('Hello from a car');
-    }
-}
+    let pairsRemaining = 26;
+    let count = 0;
 
-
-function printable(constructorFn: Function) {
-    constructorFn.prototype.print = function() {
-        console.log(this);
-    }
-}
-
-@logging(true)
-@printable
-class Plant {
-    name = "Green Plant"
-}
-
-const plant = new Plant();
-
-// forced to cast due to typescript bug
-(<any>plant).print();
-
-
-
-// Decorator Factory
-function editable(value: boolean) {
-    return function(target: any, propName: string, descriptor: PropertyDescriptor) {
-        target;
-        propName;
-        descriptor.writable = value;
-    }
-}
-
-function overwritable(value: boolean) {
-    return function(target: any, propName: string): any {
-        target;
-        propName;
-        const newDescriptor: PropertyDescriptor = {
-            writable: value
+    while (pairsRemaining > 0) {
+        const pairFound: boolean = isMatch(pairsRemaining);
+        if (pairFound) {
+            pairsRemaining -= 1;
         }
-        return newDescriptor;
+        count++;
     }
+    return count;
 }
 
-class Project {
-    //@overwritable(false)  // breaks the whole class since we can't write to this property in the constuctor
-    projectName: string;
+const collectAverage = ():void => {
 
-    constructor(name: string) {
-        this.projectName = name;
+    let testRunCount = 100000;
+    let results: number[] = [];
+    let avg: number;
+
+    while (testRunCount > 0) {
+        let turns = runGame();
+        if(typeof turns === 'number') results.push(turns);
+        testRunCount--;
     }
 
-    // want to add some meta data to this method to make it non-overwritable
-    @editable(false)
-    calcBudget() {
-        console.log(1000);
-    }
+    if (results.length) {
+        const sum = results.reduce(function (a, b) { return a + b; });
+        avg = sum / results.length;
+    } else avg = 0;
+
+    console.log('ave:', avg);
 }
 
-const project = new Project("Super Project");
-project.calcBudget();
-// project.calcBudget = function() {
-//     console.log(2000);
-// };
-// project.calcBudget();
+collectAverage();
 
-// while this is decorator you can still use factories with parameter decorators
-function printInfo(target: any, methodName: string, paramIndex: number) {
-    console.log('Target: ', target);
-    console.log('Method Name:', methodName, );
-    console.log('Parameter Index:', paramIndex);
-}
-
-class Course {
-    name: string;
-
-    constructor(name: string) {
-        this.name= name;
-    }
-
-    printStudentNumbers(mode: string, @printInfo printAll: boolean) {
-        mode;
-        if(printAll) {
-            console.log(10000);
-        } else {
-            console.log(2000);
-        }
-    }
-}
-
-const course = new Course('Super Course');
-course.printStudentNumbers('anything', true);
-course.printStudentNumbers('anything', false);
-
-// Target:  Object { … }
-// Method Name: printStudentNumbers
-// Parameter Index: 1
-// 10000
-// 2000
